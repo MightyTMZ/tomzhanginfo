8c426ac92f8a66258c845a939e667451234ac6ba
Tom Zhang <tommyawesome08@gmail.com>
2025-01-17 18:13:42 -0500

    "my works page" refined and populated with dummy data

--- src/app/my-works/page.tsx
+++ src/app/my-works/page.tsx
@@ -2,7 +2,11 @@
 
 import React from "react";
 import styles from "./MyWorks.module.css";
-import { projects } from "@/data/works";
+import {
+  coreProjects,
+  protoTypes_Experiments,
+  tools_Integrations,
+} from "@/data/works";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -10,13 +14,69 @@ const MyWorks = () => {
   return (
     <section className={styles.myWorksPage}>
       <div className={styles.container}>
-        <h1 className={styles.sectionTitle}>My Works</h1>
         <p className={styles.description}>
-          Explore my projects and see how I've brought my ideas to life. Each
-          project highlights my dedication to innovation and technology.
+          Here are some of my most significant works: projects, prototypes,
+          experiments, tools, and integrations...
         </p>
+        <h1 className={styles.sectionTitle}>Projects</h1>
         <div className={styles.projectGrid}>
-          {projects.map((project) => (
+          {coreProjects.map((project) => (
+            <div key={project.id} className={styles.projectCard}>
+              <h2 className={styles.projectTitle}>{project.title}</h2>
+              <Image
+                src={project.featureImage}
+                alt={project.title}
+                className={styles.projectFeatureImage}
+              />
+              <p className={styles.projectDescription}>{project.description}</p>
+              <div className={styles.projectLinks}>
+                {project.links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.linkIcon}
+                    aria-label={link.label}
+                  >
+                    {link.icon}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <h1 className={styles.sectionTitle}>Prototypes and Experiments</h1>
+        <div className={styles.projectGrid}>
+          {protoTypes_Experiments.map((project) => (
+            <div key={project.id} className={styles.projectCard}>
+              <h2 className={styles.projectTitle}>{project.title}</h2>
+              <Image
+                src={project.featureImage}
+                alt={project.title}
+                className={styles.projectFeatureImage}
+              />
+              <p className={styles.projectDescription}>{project.description}</p>
+              <div className={styles.projectLinks}>
+                {project.links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.linkIcon}
+                    aria-label={link.label}
+                  >
+                    {link.icon}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <h1 className={styles.sectionTitle}>Tools and Integration</h1>
+        <div className={styles.projectGrid}>
+          {tools_Integrations.map((project) => (
             <div key={project.id} className={styles.projectCard}>
               <h2 className={styles.projectTitle}>{project.title}</h2>
               <Image
--- src/data/works.tsx
+++ src/data/works.tsx
@@ -1,7 +1,7 @@
 import { FaGlobe, FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import placeholderImage from "../../public/placeholder.webp";
 
-export const projects = [
+export const coreProjects = [
   {
     id: 1,
     title: "OrbitView",
@@ -57,6 +57,147 @@ export const projects = [
         icon: <FaExternalLinkAlt />,
         url: "https://ollama-guide.com",
         label: "Read More",
+      },
+    ],
+  },
+];
+
+export const protoTypes_Experiments = [
+  {
+    id: 1,
+    title: "Prototype Experiment 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Prototype Experiment 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Prototype Experiment 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Prototype Experiment 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Prototype Experiment 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Prototype Experiment 6",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+];
+
+export const tools_Integrations = [
+  {
+    id: 1,
+    title: "Integration 1",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Integration 2",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Integration 3",
+    featureImage: placeholderImage,
+    description:
+      "A next-generation social media app fostering genuine connections and immersive learning.",
+    links: [
+      { icon: <FaGlobe />, url: "https://orbitview.com", label: "Live Site" },
+      {
+        icon: <FaGithub />,
+        url: "https://github.com/tomzhang/orbitview",
+        label: "GitHub",
       },
     ],
   },
