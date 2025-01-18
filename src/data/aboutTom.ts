e16f4875bc11db5d5b759f7144d76c17c0284d46
Tom Zhang <tommyawesome08@gmail.com>
2025-01-18 16:39:46 -0500

    added electrical engineering

--- src/data/aboutTom.ts
+++ src/data/aboutTom.ts
@@ -1,6 +1,6 @@
 export const aboutTitle = `Hello, I'm Tom Zhang. I'm passionate about technology and building
               innovative solutions to solve real-world problems.`;
-export const aboutDescription = `With a deep interest in software development, AI, and
+export const aboutDescription = `With a deep interest in software development, electrical engineering, AI, and
               entrepreneurship, I’ve embarked on a journey to revolutionize
               industries through tech. I believe in continuous learning and
               pushing boundaries to make an impact on the world.`;
