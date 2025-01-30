02598a8019ce2183396b4c32927314d0bfc4c610
Tom Zhang <tommyawesome08@gmail.com>
2025-01-30 14:13:39 -0500

    hero background updated to video and added a custom scroll bar color

--- src/components/SlideAnimationButton/SlideAnimationButton.tsx
+++ src/components/SlideAnimationButton/SlideAnimationButton.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from "./SlideAnimationButton.module.css";
 
-const SlideAnimationButton = ({
-  text,
-  onClick,
-}: {
+interface SlideAnimationButtonProps {
   text: string;
+  color: string;
   onClick: () => void;
-}) => {
+}
+
+const SlideAnimationButton = ({ text, color, onClick }: SlideAnimationButtonProps) => {
   return (
     <button className={styles["button"]} onClick={onClick}>
       <span className={styles["button_lg"]}>
-        <span className={styles["button_sl"]}></span>
+        <span className={styles["button_sl"]} style={{ background: color }}></span>
         <span className={styles["button_text"]}>{text}</span>
       </span>
     </button>
