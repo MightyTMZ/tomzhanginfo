2b610f25e46b358df00db3676cda660d1aabf1b7
Tom Zhang <tommyawesome08@gmail.com>
2025-02-20 09:45:07 -0500

    chip on my shoulder quote

--- src/data/favouriteQuote.ts
+++ src/data/favouriteQuote.ts
@@ -1,2 +1,2 @@
-export const quoteContent = 'The best projects are likely to be overlooked, not trumpeted by a crowd; the best problems to work on are often the ones nobody else even tries to solve.';
-export const quoteAuthor = 'Peter Thiel, Co-Founder and Former CEO of PayPal';
+export const quoteContent = "I often don't like to talk about it. I let my success speak since it's deafening already by itself.";
+export const quoteAuthor = 'Tom Zhang';
