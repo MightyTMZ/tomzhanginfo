092ce5fde52ce9a69f867771e1173e6713c20b3a
Tom Zhang <tommyawesome08@gmail.com>
2025-03-21 21:35:30 -0400

    resume update (again)

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -26,7 +26,7 @@ const Resume = () => {
           <a href="/connect-with-me"><span className="text-blue-500">reach out to me</span></a>
         </p>
         <iframe
-          src="/Tom_Zhang_Resume.pdf"
+          src="/Tom Zhang - Resume.pdf"
           width="75%"
           height="800px"
           title="My Resume"
