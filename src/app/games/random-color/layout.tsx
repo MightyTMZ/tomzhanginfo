cf7938225ceb8b13ec30597966a721fc2543f83b
Tom Zhang <tommyawesome08@gmail.com>
2025-07-27 15:49:02 -0400

    random color page title update

--- src/app/games/random-color/layout.tsx
+++ src/app/games/random-color/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 
 
 export const metadata: Metadata = {
-  title: "Tom Zhang - Games - Random Color",
+  title: "Tom Zhang - Random Color",
   description: "Click the screen to change to a new color!",
 };
 
