02f32792d8e3df3ad6bd4d7313e68569af33fb0e
Tom Zhang <tommyawesome08@gmail.com>
2025-08-04 12:41:14 -0400

    full url image/audio?

--- src/app/games/saleem/page.tsx
+++ src/app/games/saleem/page.tsx
@@ -2,25 +2,33 @@
 
 import React, { useState } from "react";
 
-const images = [
-  "leem1.png", 
-  "leem2.png", 
-  "leem3.png", 
-  "leem3.png", 
-  "leem4.png", 
-  "leem5.png", 
-  "leem6.png", 
-];
-const sounds = ["unplug_your_ears.m4a", "join_me_on_teams.m4a", "alright_heres_the_question.m4a"];
+const bucketURL = process.env.MEDIA_BUCKET;
 
-const bucketURL = process.env.MEDIA_BUCKET
+const images = [
+  "leem1.png",
+  "leem2.png",
+  "leem3.png",
+  "leem3.png",
+  "leem4.png",
+  "leem5.png",
+  "leem6.png",
+];
+const sounds = [
+  "unplug_your_ears.m4a",
+  "join_me_on_teams.m4a",
+  "alright_heres_the_question.m4a",
+];
+
+const fullImageURLs = images.map((img) => `${bucketURL}/${img}`);
+const fullSoundURLs = sounds.map((snd) => `${bucketURL}/${snd}`);
+
 
 const MrSaleem: React.FC = () => {
-  const [currentImage, setCurrentImage] = useState<string>(images[0]);
+  const [currentImage, setCurrentImage] = useState<string>(fullImageURLs[0]);
 
   const handleImageClick = () => {
     // Choose a random sound and play it
-    const randomSound = new Audio(`${bucketURL}/${randomItem(sounds)}`);
+    const randomSound = new Audio(randomItem(fullSoundURLs));
     randomSound.play();
 
     // Swap to a new random image
@@ -32,26 +40,31 @@ const MrSaleem: React.FC = () => {
   };
 
   const playAudio = (id: number) => {
-    const sound = new Audio(`${bucketURL}/${sounds[id]}`);
+    const audioSource = sounds[id];
+    const sound = new Audio(audioSource);
+    console.log(audioSource);
     sound.play();
-  }
+  };
 
   return (
     <div className="m-10 text-center">
       <div className="text-2xl font-bold mb-4">Hello I'm Mr. Saleem!</div>
       <img
-        src={`${bucketURL}/${currentImage}`}
+        src={currentImage}
         alt="Mr. Saleem"
         onClick={handleImageClick}
         className="cursor-pointer mx-auto w-64 h-auto transition-transform duration-200 hover:scale-105"
       />
 
-      <div className="mt-8 space-x-4">
-        {/* "Fire up Teams", "Yes!", "Okay!" */}
-        {["Unplug your ears!", "Join me on Teams!", "Alright, here's the question..."].map((label, audio_id) => (
+      <div className="mt-8 space-x-4 mb-15">
+        {[
+          "Unplug your ears!",
+          "Join me on Teams!",
+          "Alright, here's the question...",
+        ].map((label, audio_id) => (
           <button
             key={audio_id}
-            className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-xl shadow-lg"
+            className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded-xl shadow-lg mb-4"
             onClick={() => playAudio(audio_id)}
           >
             {label}
