effdf9490ec2d7e976e900815c60476342245444
Tom Zhang <tommyawesome08@gmail.com>
2025-08-26 14:20:58 -0400

    New about paragraph

--- src/data/aboutTom.ts
+++ src/data/aboutTom.ts
@@ -1,10 +1,7 @@
 export const aboutTitle = `Hello, I'm Tom Zhang. I'm passionate about technology and building
               innovative solutions to solve real-world problems.`;
-export const aboutDescription = `With a deep interest in software development, electrical engineering, AI, and
+export const aboutDescription = `With a deep interest in software engineering, full stack development, AI, electrical engineering and
               entrepreneurship, I have embarked on a journey to explore industries through tech. As an ENTJ, I thrive on strategic thinking, leadership, and pushing ambitious ideas forward. I believe in continuous learning and
               pushing boundaries to make an impact on the world.
-              
-              Come time to destress, I love to exercise in the gym and play lots of outdoor
-              sports like baseball and ultimate frisbee. 
               
               `;
